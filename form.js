fdd924a550bdd60edba9995053abf29d11008b82
raagzi <114258169+raagzi@users.noreply.github.com>
2023-06-29 15:41:20 +0530

    Update form.js

--- form.js
+++ form.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
+import './form.css'; // Import the CSS file
 
-const MyForm = () => {
+const Form = () => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
     message: '',
+    country: '',
   });
 
   const handleChange = (e) => {
@@ -13,44 +15,70 @@ const MyForm = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Handle form submission
+    // Process the form data
     console.log(formData);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Name:
-        <input
-          type="text"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-        />
-      </label>
-      <br />
-      <label>
-        Email:
-        <input
-          type="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-        />
-      </label>
-      <br />
-      <label>
-        Message:
-        <textarea
-          name="message"
-          value={formData.message}
-          onChange={handleChange}
-        ></textarea>
-      </label>
-      <br />
-      <button type="submit">Submit</button>
-    </form>
+    <div className="form-container"> {/* Apply the CSS class to the main container */}
+      <h2>Contact Form</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name
+          <input
+            type="text"
+            name="name"
+            placeholder='Enter your name'
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <br />
+        <label>
+          Email
+          <input
+            type="email"
+            name="email"
+            placeholder='Enter your email'
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <br />
+        <label>
+          Message
+          <textarea
+            name="message"
+            placeholder='Enter your message'
+            value={formData.message}
+            onChange={handleChange}
+            required
+          ></textarea>
+        </label>
+        <br />
+        <label>
+          Country
+          <select
+            name="country"
+            value={formData.country}
+            onChange={handleChange}
+            required
+          >
+            <option value="">Select a country</option>
+            <option value="India">India</option>
+            <option value="USA">USA</option>
+            <option value="Canada">Canada</option>
+            <option value="UK">UK</option>
+            <option value="Germany">Germany</option>
+          </select>
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   );
 };
 
-export default MyForm;
+export default Form;
